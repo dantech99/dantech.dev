f9e6d4e3274d81017338708e549052487907d636
DandevMS <xxdndkxx@gmail.com>
2022-11-19 01:44:33 -0500

    terminado la maquetacion del menu y se adapto para ser responsive, se configuro el formulario de contacto para poder recibir mensajes

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,8 @@
-AOS.init();
+
+
+var menu = document.getElementById('menu');
+export function toggleMenu() {
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('w-full');
+    menu.classList.toggle('h-screen');
+}
