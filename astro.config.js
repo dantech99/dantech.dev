2ad51a969030a702801c8ea38a0781aef19e638d
DandevMS <xxdndkxx@gmail.com>
2022-11-27 16:57:03 -0500

    add vercel deploy

--- astro.config.js
+++ astro.config.js
@@ -5,6 +5,11 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
