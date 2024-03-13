6e174523ca7cd637f831810b7baf25a32a7bfacd
DanTech99 <antoniomcs21@gotmail.com>
2024-03-12 20:29:47 -0500

    se eliminaron typos innecesariios

--- src/utils/types/site.d.ts
+++ src/utils/types/site.d.ts
@@ -22,39 +22,4 @@ interface Social {
   link: string
 }
 
-interface Work {
-  label: string
-  title: string
-  description: string
-  link: string
-}
-
-interface Projects {
-  title: string
-  tech: Array<Tech>
-  description: string
-  links: Array<Links>
-  image: string
-}
-
-interface Tech {
-  name:  string
-  icon: Array<string>
-}
-
-interface Links {
-  name: string
-  url: string
-}
-
-interface Skills {
-  name: string
-}
-
-interface About {
-  title: string
-  description:string
-  image: string
-}
-
 
