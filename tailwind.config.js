f70b4df4545d386ff7bac9e780bb0cb2769357a8
DanTech99 <antoniomcs21@gotmail.com>
2024-08-14 21:17:50 -0500

    chore: Update Tailwind CSS configuration and add new SVG icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations';
 module.exports = {
   darkMode: 'class', // or 'media' or 'class' 
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {},
   },
-  
+  plugins: [animations],
 };
-
-
