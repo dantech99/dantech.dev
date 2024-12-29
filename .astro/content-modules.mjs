7100b9d6b41cba04a58a67f1c605acb8cd369f8b
dantech99 <dantech.dev@hotmail.com>
2024-12-29 17:55:30 -0500

    feature: update blog layout and SEO metadata, add article template snippets, and enhance article management

--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,1 +1,4 @@
-export default new Map();
+
+export default new Map([
+["src/content/articles/EcmaScript-y-sus-versiones.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Farticles%2FEcmaScript-y-sus-versiones.mdx&astroContentModuleFlag=true")]]);
+		
