94816951c7dbfa230480afd9ca3c1b9e50cec50d
dantech99 <dantech.dev@hotmail.com>
2025-02-15 15:34:10 -0500

    feat: add projects content collection and update project-related components

--- src/content/config.ts
+++ src/content/config.ts
@@ -12,6 +12,30 @@ const articles = defineCollection({
   })
 })
 
+const projects = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    img: z.string(),
+    date: z.date(),
+    links: z.array(z.object({
+      name: z.string(),
+      url: z.string().url(),
+      icon: z.string()
+    })),
+    tags: z.array(z.string()),
+    tech: z.array(z.object({
+      name: z.string(),
+      icon: z.string()
+    })),
+    author: z.string(),
+    authorImg: z.string(),
+    authorUrl: z.string().url(),
+  })
+})
+
 export const collections = {
-  'articles': articles
+  'articles': articles,
+  'projects': projects
 }
